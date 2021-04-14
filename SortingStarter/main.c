9496149f5b2e71afa83a17936bdd6e2ca693454c
Chandler Glowicki <glowickc@eos11.cis.gvsu.edu>
2021-04-14 19:04:56 -0400

    Unfinished

--- SortingStarter/main.c
+++ SortingStarter/main.c
@@ -1,6 +1,8 @@
 #include "file_lib.h"
 #include <stdio.h>
 #include "sorter.h"
+#include <string.h>
+
 /*
  * Read the file from the command-line.
  * Usage:
@@ -8,37 +10,30 @@
  *
  */
 
-void filler(char** arr, int size){
-
-	for(int i=0; i<size; ++i){
-		*arr[i]=cont[i];
-	}	
-}
-
 
 int main(int argc, char** argv){
 	// Read the original file.
 	
 	char* content;
 	size_t size=load_file(argv[1],&content);
+	
+	
 
 	printf("\n%lu bytes read from the file\n",size);	
-	
-//	for( int i =0; i<100; ++i){
-//		printf("%c",content[i]);
-//	}
-				
+		
 	load_file(argv[1], &content);	
-	filler(&content,size);
+	
 
 	// Sort the file with the function you wrote.
-	
-	printf("%c\n",content[0]);	
 	sort(&content , size);
+	
+	//print the 5th word??
+	printf("\n%s", content);
 
 	// Write out the new file.
 	save_file(argv[2], content , size);	
-	}
+
+}
 
 
 // You can see if your file worked correctly by using the
--- SortingStarter/sorter.c
+++ SortingStarter/sorter.c
@@ -2,16 +2,48 @@
 #include "sorter.h"
 #include "file_lib.h"
 #include <stdio.h>
+#include <string.h>
+
+
 void sort(char** contents, int size){
-	printf("\n%s\n\n\n\n\n",contents[0]);
-	for( int i=1; i<size; ++i){
-	//	char tempi=i;	
-		/**	while ( ( (*contents[tempi]) ) <  (*contents[tempi-1]) )  {
-				char temp = (*contents[tempi-1]);
-				(*contents[tempi-1]) = (*contents[tempi]);
-				(*contents[tempi]) = temp;
-				--tempi;
-			}
-	*/	}
+	
+	//Designate memory for the array
+	char** arr = malloc(sizeof(char*)*size);
+	
+	int i=0;
+	int numlines=0;
+
+	//Putting the file into an array
+	char* tok = strtok(*contents, "\n:");
+	while( tok != NULL ){
+		arr[numlines]=tok;
+		
+		//Testing to make sure the array is created correctly
+		//	printf("%s\n", arr[numlines]);
+		
+		tok = strtok(NULL, "\n:");
+		++numlines;
 	}
+
+	printf("\n%d lines read from the file",numlines);
+
+/**	for( int i=numlines; i>0; --i){
+		int tempi=i;	
+		while (i > 0) {
+			while (arr[i]  < arr[tempi-1]) {
+				if(tempi>0){
+					char* temp = arr[tempi-1];
+					arr[tempi-1] = arr[tempi];
+					arr[tempi] = temp;
+					--tempi;
+				}
+			}	
+			--i;
+			}
+		}
+*/	contents = malloc(sizeof(char*)*size);
+	for(int i=0; i<numlines; ++i) {
+		contents = arr;
+	}
+}
 		
